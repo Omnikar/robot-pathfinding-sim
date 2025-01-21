a1f6566ceb5fdd697aab6d92c72ab9de2cf053e4
Dguto9 <dillongutowski@gmail.com>
2025-01-20 19:10:16 -0500

    Simulate robot auto behavior

--- src/displayauto.rs
+++ src/displayauto.rs
@@ -1,8 +1,8 @@
-use crate::graph::{FieldGraph, SpatialGraph};
+use crate::graph::FieldGraph;
 use crate::AutoPath;
 use bevy::{color::palettes::css::*, prelude::*};
-use bevy_prototype_lyon::prelude::*;
 use serde::{Deserialize, Serialize};
+use std::collections::HashMap;
 
 const FILL: Srgba = WHITE;
 const STROKE: Srgba = BLUE_VIOLET;
@@ -19,15 +19,29 @@ impl Plugin for AutoPlugin {
     }
 }
 
+#[derive(Resource, Serialize, Deserialize)]
+pub struct Waypoints(HashMap<String, Vec2>);
+impl std::ops::Deref for Waypoints {
+    type Target = HashMap<String, Vec2>;
+    fn deref(&self) -> &Self::Target {
+        &self.0
+    }
+}
+impl std::ops::DerefMut for Waypoints {
+    fn deref_mut(&mut self) -> &mut Self::Target {
+        &mut self.0
+    }
+}
+
 fn init_waypoints(mut commands: Commands) {
     type E = Box<dyn std::error::Error>;
     let mut wpts: Waypoints = serde_json::from_str(include_str!("../assets/field_waypoints.json"))
         .map_err(E::from)
         .unwrap();
-    for node in wpts.wpts.iter_mut() {
-        node.pos.y *= -1.0;
-        std::mem::swap(&mut node.pos.x, &mut node.pos.y);
-        node.pos += crate::graph::ORIGIN_OFFSET;
+    for node in wpts.values_mut() {
+        node.y *= -1.0;
+        std::mem::swap(&mut node.x, &mut node.y);
+        *node += crate::graph::ORIGIN_OFFSET;
     }
     commands.insert_resource(wpts);
 }
@@ -51,17 +65,6 @@ pub struct DrawnAuto {
     pub edges: Vec<Entity>,
 }
 
-#[derive(Resource, Clone, Serialize, Deserialize)]
-pub struct Waypoints {
-    pub wpts: Vec<Waypoint>,
-}
-
-#[derive(Clone, Serialize, Deserialize)]
-pub struct Waypoint {
-    pub name: String,
-    pub pos: Vec2,
-}
-
 #[derive(Resource, Default)]
 struct DrawnWaypoints {
     nodes: Vec<Entity>,
@@ -72,9 +75,9 @@ fn plot_waypoints(
     mut drawn: ResMut<DrawnWaypoints>,
     mut commands: Commands,
 ) {
-    for node in graph.wpts.iter() {
+    for node in graph.iter() {
         drawn.nodes.push(crate::graph::draw_node(
-            node.pos,
+            *node.1,
             STROKE,
             FILL,
             &mut commands,
@@ -89,28 +92,30 @@ fn draw_auto(
     mut drawn: ResMut<DrawnAuto>,
     mut commands: Commands,
 ) {
-    let mut prev = match_waypoint("robostart", &wpts);
-    for current in auto.wpts.iter().map(|x| match_waypoint(&x, &wpts)) {
-        let Some(path) = crate::robot::compute_path(prev.pos, current.pos, &graph.sg) else {
-            return;
-        };
-        let mut prev_1 = prev.pos;
-        for current_1 in path.iter() {
-            drawn.edges.push(crate::graph::draw_edge(
-                prev_1,
-                *current_1,
-                STROKE,
-                &mut commands,
-            ));
-            prev_1 = *current_1;
-        }
-        prev = current;
+    let path = gen_auto_path(&auto, &wpts, &graph);
+    for edge in path.windows(2) {
+        drawn.edges.push(crate::graph::draw_edge(
+            edge[0],
+            edge[1],
+            STROKE,
+            &mut commands,
+        ));
     }
 }
 
-fn match_waypoint<'a>(auto: &str, wpts: &'a Waypoints) -> &'a Waypoint {
-    wpts.wpts
+pub fn gen_auto_path(auto: &Auto, wpts: &Waypoints, graph: &FieldGraph) -> Vec<Vec2> {
+    let mut path: Vec<Vec2> = Vec::new();
+    path.push(*wpts.get("robostart").expect("Where you starting???"));
+    for (i, current) in auto
+        .wpts
         .iter()
-        .find(|x| x.name == auto)
-        .unwrap_or_else(|| panic!("No waypoint found {}", auto))
+        .map(|x| wpts.get(x).expect("nada"))
+        .enumerate()
+    {
+        let mut subpath =
+            crate::robot::compute_path(*path.last().expect("nothin in this"), *current, &graph.sg)
+                .expect("No path found, silly");
+        path.append(&mut subpath);
+    }
+    return path;
 }
